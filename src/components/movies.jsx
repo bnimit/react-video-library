3f9353a0757d36b686f1db107d46dfa66ba01e16
Nimit Bhandari <nbhandari@mavenlink.com>
2020-02-22 10:03:32 -0700

    Adding pagination, like behavior to the video library

---
+++ src/components/movies.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import Pagination from "./common/pagination";
+import paginate from "../utils/paginate";
+import Like from "./common/like";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+    pageSize: 4,
+    currentPage: 1
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+  render() {
+    const { pageSize, currentPage, movies: allmovies } = this.state;
+    const { length: count } = this.state.movies;
+    const movies = paginate(allmovies, currentPage, pageSize);
+
+    if (count === 0)
+      return (
+        <p>
+          <strong>There are no movies in the database!</strong>
+        </p>
+      );
+    return (
+      <React.Fragment>
+        <p>
+          Showing <strong> {count} </strong> movies from the database
+        </p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+              <th>Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          itemCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
